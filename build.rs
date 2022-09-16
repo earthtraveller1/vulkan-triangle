c68b214c2301993cf7a119c58c8d9a35b018ba81
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-09-16 18:15:40 -0400

    Created the FFI bindings for Vulkan.

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::{env, path::PathBuf};
+
+fn main() {
+    println!("cargo:rustc-link-lib=vulkan");
+    println!("cargo:rerun-if-changed=src/ffi/vulkan.h");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    bindgen::Builder::default()
+        .header("src/ffi/vulkan.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings for src/ffi/vulkan.h")
+        .write_to_file(out_path.join("vulkan-ffi.rs"))
+        .expect("Couldn't write bindings to file.");
+}
