4f497f2f8abea23e6a33fff69aec773431e2a630
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-09-16 19:12:20 -0400

    Tested instance creation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use vulkan_triangle::backend::Instance;
+
 fn main() {
-    println!("Hello, world!");
+    Instance::enumerate_extension_names()
+        .iter()
+        .for_each(|extension| println!("[INFO]: Found Vulkan Instance extension {}", extension));
+
+    // let instance =
+    //     Instance::new("Vulkan Triangle", 0, 1, 0).expect("Failed to create the Vulkan instance.");
 }
