2f9a0c56762fe07e17850ef32e0babc206b7fa6b
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-09-18 11:07:28 -0400

    Used the window class.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use vulkan_triangle::backend::Instance;
+use vulkan_triangle::Window;
 
 fn main() {
+    let mut window = Window::new(800, 600, "Vulkan Triangle", None);
+
     Instance::enumerate_extension_names()
         .iter()
         .for_each(|extension| println!("[INFO]: Found Vulkan Instance extension {}", extension));
@@ -11,4 +14,10 @@ fn main() {
     instance
         .create_debug_messenger()
         .expect("[ERROR]: Failed to create the debug messenger!");
+
+    window.show();
+
+    while window.is_open() {
+        window.update();
+    }
 }
