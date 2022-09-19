86bdc6ee5fd80132ce050abac288e8c123e21966
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-09-19 18:01:24 -0400

    Added a way to find queue families.

--- src/backend/physical_device.rs
+++ src/backend/physical_device.rs
@@ -1,6 +1,7 @@
 use super::ffi::vulkan::*;
 use core::mem::MaybeUninit;
-use std::ffi::CStr;
+use std::{ffi::CStr, ptr::null_mut, convert::TryInto};
+use super::Surface;
 
 pub struct PhysicalDevice {
     pub(super) raw_handle: VkPhysicalDevice,
@@ -17,6 +18,25 @@ pub enum DeviceType {
 pub struct Properties {
     pub name: String,
     pub device_type: DeviceType
+}
+
+pub struct QueueFamilyIndices {
+    pub graphics_family: Option<u32>,
+    pub present_family: Option<u32>
+}
+
+impl QueueFamilyIndices {
+    pub fn is_complete(&self) -> bool {
+        if let Some(_) = self.graphics_family {
+            if let Some(_) = self.present_family {
+                true
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
 }
 
 impl PhysicalDevice {
@@ -41,6 +61,44 @@ impl PhysicalDevice {
                 }
             }
         }
+    }
+    
+    pub fn find_queue_families(&self, surface: &Surface) -> QueueFamilyIndices {
+        let mut queue_family_count = 0;
+        unsafe { vkGetPhysicalDeviceQueueFamilyProperties(self.raw_handle, &mut queue_family_count, null_mut()) };
+        
+        let mut queue_family_properties = Vec::with_capacity(queue_family_count.try_into().unwrap());
+        unsafe { 
+            vkGetPhysicalDeviceQueueFamilyProperties(self.raw_handle, &mut queue_family_count, queue_family_properties.as_mut_ptr());
+            queue_family_properties.set_len(queue_family_count.try_into().unwrap());
+        }
+        
+        let mut indices = QueueFamilyIndices { graphics_family: None, present_family: None };
+        
+        for i in 0..queue_family_properties.len() {
+            {
+                let a: u32 = queue_family_properties[i].queueFlags;
+                let b: u32 = VK_QUEUE_GRAPHICS_BIT.try_into().unwrap();
+                let c: u32 = a & b;
+                
+                if c > 0 {
+                    indices.graphics_family = Some(i.try_into().unwrap());
+                }
+            }
+            
+            if indices.is_complete() {
+                break;
+            }
+            
+            let mut present_support = VK_FALSE;
+            unsafe { vkGetPhysicalDeviceSurfaceSupportKHR(self.raw_handle, i.try_into().unwrap(), surface.raw_handle, &mut present_support) };
+            
+            if present_support == VK_TRUE {
+                indices.present_family = Some(i.try_into().unwrap());
+            }
+        }
+        
+        indices
     }
 }
 
