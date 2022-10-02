a94c056d64ef0e9b64a7e68f8b8eb2a239866e08
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-10-02 14:54:31 -0400

    Added VK_MAKE_API_VERSION.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 #[allow(nonstandard_style)]
 pub mod vulkan {
     include!(concat!(env!("OUT_DIR"), "/vulkan-ffi.rs"));
+    
+    pub fn VK_MAKE_API_VERSION(variant: u32, major: u32, minor: u32, patch: u32) -> u32 {
+        (((variant)) << 29) | (((major)) << 22) | (((minor)) << 12) | ((patch))
+    }
 }
