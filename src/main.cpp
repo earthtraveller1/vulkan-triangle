dc31e61e4d2ac6daa44a9ad18896b622d1704184
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-10-09 19:50:24 -0400

    Created a Vulkan instance.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <GLFW/glfw3.h>
 #include <fmt/format.h>
+#include <vulkan/vulkan.h>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -10,12 +11,48 @@
 namespace
 {
 
-const uint16_t WINDOW_WIDTH = 1024;
-const uint16_t WINDOW_HEIGHT = 768;
+constexpr uint16_t WINDOW_WIDTH = 1024;
+constexpr uint16_t WINDOW_HEIGHT = 768;
 
 void glfw_error_callback(int p_error_code, const char *p_message)
 {
     fmt::print("[GLFW ERROR {}]: {}\n", p_error_code, p_message);
+}
+
+VkInstance create_instance()
+{
+    VkApplicationInfo application_info
+    {
+        .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pNext = nullptr,
+        .pApplicationName = "Vulkan Triangle",
+        .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = nullptr,
+        .engineVersion = 0,
+        .apiVersion = VK_API_VERSION_1_2
+    };
+    
+    VkInstanceCreateInfo create_info 
+    {
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .pApplicationInfo = &application_info,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = nullptr,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = nullptr
+    };
+    
+    VkInstance instance;
+    VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
+    if (result != VK_SUCCESS)
+    {
+        fmt::print("[FATAL ERROR]: Failed to create the Vulkan instance. Vulkan Error {}", result);
+        std::exit(EXIT_FAILURE);
+    }
+    
+    return instance;
 }
 
 // The actual main function
@@ -26,6 +63,8 @@ int real_main()
         fmt::print("[FATAL ERROR]: Failed to initialize GLFW.\n");
         return EXIT_FAILURE;
     }
+    
+    VkInstance instance = create_instance();
 
     glfwSetErrorCallback(glfw_error_callback);
 
@@ -48,6 +87,8 @@ int real_main()
     {
         glfwPollEvents();
     }
+    
+    vkDestroyInstance(instance, nullptr);
 
     glfwTerminate();
     return EXIT_SUCCESS;
