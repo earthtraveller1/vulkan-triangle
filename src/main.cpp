474df7f8791b578757dd15cd064b8499f2db0a77
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-10-14 20:05:34 -0400

    Enabled vulkan validation.

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,42 @@
 
 #include <cstdlib>
 
+#include <vector>
+
+#define LOAD_VK_FUNCTION(function, instance)                                   \
+    const auto hello56721_##function = reinterpret_cast<PFN_##function>(       \
+        vkGetInstanceProcAddr(instance, #function))
+
 namespace
 {
 
-constexpr uint16_t WINDOW_WIDTH = 1024;
-constexpr uint16_t WINDOW_HEIGHT = 768;
+const uint16_t WINDOW_WIDTH = 1024;
+const uint16_t WINDOW_HEIGHT = 768;
+
+const bool ENABLE_VALIDATION = true;
+
+VkBool32 debug_messenger_callback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT, VkDebugUtilsMessageTypeFlagsEXT,
+    const VkDebugUtilsMessengerCallbackDataEXT *p_callback_data, void *)
+{
+    fmt::print("[VULKAN]: {}\n", p_callback_data->pMessage);
+
+    return VK_FALSE;
+}
+
+const VkDebugUtilsMessengerCreateInfoEXT DEBUG_MESSENGER_CREATE_INFO{
+    .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+    .pNext = nullptr,
+    .flags = 0,
+    .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT |
+                       VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                       VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+                       VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT,
+    .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                   VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                   VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+    .pfnUserCallback = debug_messenger_callback,
+    .pUserData = nullptr};
 
 void glfw_error_callback(int p_error_code, const char *p_message)
 {
@@ -30,15 +61,45 @@ VkInstance create_instance()
         .engineVersion = 0,
         .apiVersion = VK_API_VERSION_1_2};
 
-    VkInstanceCreateInfo create_info{.sType =
-                                         VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-                                     .pNext = nullptr,
-                                     .flags = 0,
-                                     .pApplicationInfo = &application_info,
-                                     .enabledLayerCount = 0,
-                                     .ppEnabledLayerNames = nullptr,
-                                     .enabledExtensionCount = 0,
-                                     .ppEnabledExtensionNames = nullptr};
+    uint32_t glfw_vulkan_extension_count;
+    const char **glfw_vulkan_extensions =
+        glfwGetRequiredInstanceExtensions(&glfw_vulkan_extension_count);
+
+    std::vector<const char *> enabled_extensions(glfw_vulkan_extensions,
+                                             glfw_vulkan_extensions +
+                                                 glfw_vulkan_extension_count);
+    if (ENABLE_VALIDATION)
+    {
+        enabled_extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    }
+
+    std::vector<const char *> enabled_layers;
+    if (ENABLE_VALIDATION)
+    {
+        enabled_layers.push_back("VK_LAYER_KHRONOS_validation");
+    }
+    
+    fmt::print("[INFO]: Enabling the following extensions:\n");
+    for (const auto& extension: enabled_extensions)
+    {
+        fmt::print("\t{}\n", extension);
+    }
+
+    VkInstanceCreateInfo create_info{
+        .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .pApplicationInfo = &application_info,
+        .enabledLayerCount = static_cast<uint32_t>(enabled_layers.size()),
+        .ppEnabledLayerNames = enabled_layers.data(),
+        .enabledExtensionCount =
+            static_cast<uint32_t>(enabled_extensions.size()),
+        .ppEnabledExtensionNames = enabled_extensions.data()};
+
+    if (ENABLE_VALIDATION)
+    {
+        create_info.pNext = &DEBUG_MESSENGER_CREATE_INFO;
+    }
 
     VkInstance instance;
     VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
@@ -53,6 +114,23 @@ VkInstance create_instance()
     return instance;
 }
 
+VkDebugUtilsMessengerEXT create_debug_messenger(VkInstance p_instance)
+{
+    LOAD_VK_FUNCTION(vkCreateDebugUtilsMessengerEXT, p_instance);
+    VkDebugUtilsMessengerEXT debug_messenger;
+    VkResult result = hello56721_vkCreateDebugUtilsMessengerEXT(
+        p_instance, &DEBUG_MESSENGER_CREATE_INFO, nullptr, &debug_messenger);
+    if (result != VK_SUCCESS)
+    {
+        fmt::print("[FATAL ERROR]: Failed to create the debug messenger. "
+                   "Vulkan error {}",
+                   result);
+        std::exit(EXIT_FAILURE);
+    }
+
+    return debug_messenger;
+}
+
 // The actual main function
 int real_main()
 {
@@ -63,6 +141,12 @@ int real_main()
     }
 
     VkInstance instance = create_instance();
+
+    VkDebugUtilsMessengerEXT debug_messenger;
+    if (ENABLE_VALIDATION)
+    {
+        debug_messenger = create_debug_messenger(instance);
+    }
 
     glfwSetErrorCallback(glfw_error_callback);
 
@@ -84,6 +168,13 @@ int real_main()
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+    }
+
+    if (ENABLE_VALIDATION)
+    {
+        LOAD_VK_FUNCTION(vkDestroyDebugUtilsMessengerEXT, instance);
+        hello56721_vkDestroyDebugUtilsMessengerEXT(instance, debug_messenger,
+                                                   nullptr);
     }
 
     vkDestroyInstance(instance, nullptr);
